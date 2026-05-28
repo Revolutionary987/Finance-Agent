9b0b0ee108519d896490a6d2442ef28d9a57c722
Revolutionary987 <busygodyo@gmail.com>
2026-05-28 18:31:49 +0530

    Added grading

--- brain.py
+++ brain.py
@@ -1,12 +1,14 @@
 from langgraph.graph import StateGraph,START,END
 from langchain_google_genai import ChatGoogleGenerativeAI
-from typing import TypedDict,Annotated,List
+from typing import TypedDict,Annotated,List,Literal
 from langgraph.graph.message import add_messages
 from langchain_core.messages import BaseMessage,HumanMessage,AIMessage
 from pydantic import BaseModel,Field
 from langgraph.store.postgres import PostgresStore
 from langgraph.store.base import BaseStore
 from retriever import Retriver
+from langchain_core.prompts import ChatPromptTemplate
+
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash",temperature=0)
 
 retriver=Retriver
@@ -26,6 +28,9 @@ class RAGSubGraph(TypedDict):
     retrieved:List[str] 
     draft:List[str]
     output:str
+    grading:str
+    structured_out:List[str]
+    answer:List[str]
 
 def output(state:MainGraph):
     output=state["output"]
@@ -68,8 +73,62 @@ The node calls the retriever function and retrives the necessary documents
 
     return {"retrived":documents}
 
+def retrieved_docs(BaseModel):
+    docs:Annotated[str,Field(description="Format the docs grade it if revelant return pass else fail")]
+
 def grade(state:RAGSubGraph):
+
+    question=state["question"]
+    current_question=question[-1].content
+    docs=state["retrieved"]
+
+    system_prompt = """You are Aegis, an elite financial auditor. 
+    Your task is to evaluate retrieved SEC 10-K document chunks.
+    Determine if the document contains facts, tables, or metrics relevant to the user's question.
+    If it is relevant, grade it pass'. If it is completely irrelevant, grade it 'fail'."""
+
+    human_prompt = """
+    Here is the user's question:
+    <user_question>
+    {question}
+    </user_question>
+
+    Here is the retrieved document chunk:
+    <retrieved_document>
+    {docs}
+    </retrieved_document>
+
+    Carefully analyze the document against the question and provide your binary score.
+    """
+    grade_prompt = ChatPromptTemplate.from_messages([
+        ("system", system_prompt),
+        ("human", human_prompt)
+    ])
+        # Create the strict grader
+    structured_grader = llm.with_structured_output(retrieved_docs)
+    grading_chain = grade_prompt | structured_grader
+    for i in range(len(docs)):
+        result=grading_chain.invoke({
+            "question": current_question, 
+            "chunks": docs,
+        })
+    state["structured_out"]=result
+    if result.binary_score == "pass":
+        return {"grading":"pass"}
+    else:
+        return {"grading":"fail"}
+
+def examiner(state:RAGSubGraph)->Literal["pass","fail"]:
+    if state["grading"]=="pass":
+        return "Generate answer"
+    else:
+        return "Rewrite"
     
-    draft=state["draft"]
-    prompt=""
+def gen_answer(state:RAGSubGraph):
     
+    possible_ans=state["structured_out"]
+    system_prompt="""You are Aegis, an elite financial auditor. 
+    Your task is to generate answer to the user's question based on retrieved SEC 10-K document chunks.
+    Don't add any extra information give the answer strictly on the basis of the retrieved chunks or if you can't find the required resource just print I don't know
+    """
+
