5afc36a2c170038653519417b3d6b6c33bcd1662
Revolutionary987 <busygodyo@gmail.com>
2026-05-28 17:28:12 +0530

    Added retrieval

--- brain.py
+++ brain.py
@@ -6,8 +6,10 @@ from langchain_core.messages import BaseMessage,HumanMessage,AIMessage
 from pydantic import BaseModel,Field
 from langgraph.store.postgres import PostgresStore
 from langgraph.store.base import BaseStore
-
+from retriever import Retriver
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash",temperature=0)
+
+retriver=Retriver
 
 class MainGraph(TypedDict):
     question:Annotated[List[BaseMessage],add_messages]
@@ -20,7 +22,8 @@ structured_llm=llm.with_structured_output(Display)
 
 class RAGSubGraph(TypedDict):
     question:Annotated[List[BaseMessage],add_messages]
-    rewritten:bool 
+    rewritten:bool
+    retrieved:List[str] 
     draft:List[str]
     output:str
 
@@ -32,8 +35,7 @@ def output(state:MainGraph):
 parent=StateGraph(MainGraph)
 child=StateGraph(RAGSubGraph)
 
-child.add_node("retriever",retrirver)
-child.add_node("reranker",reranker)
+child.add_node("retriever",retriever_graph)
 child.add_node("Grading",grade)
 child.add_node("Generate answer",gen_answer)
 child.add_node("Answer check",answer_check)
@@ -41,8 +43,7 @@ child.add_node("Rewrite",rewrite_query)
 child.add_node("output",to_parent)
 
 child.add_edge(START,"retriever")
-child.add_edge("retriever","reranker")
-child.add_edge("reranker","Grading")
+child.add_edge("retriever","Grading")
 child.add_conditional_edges("Grading",examiner)
 child.add_conditional_edges("Generate answer",check_hallucination,{"hallucination":"Generate answer", "No hallucination": "Answer check"})
 child.add_conditional_edges("Answer check",sufficient)
@@ -57,3 +58,18 @@ parent.add_edge(START,"Subgraph")
 parent.add_edge("Subgraph","Output")
 parent.add_edge("Output",END)
 
+def retriever_graph(state:RAGSubGraph):
+    """
+The node calls the retriever function and retrives the necessary documents
+    """
+    query=state["question"]
+    current_query=query[-1].content
+    documents=retriver.search.invoke(current_query)
+
+    return {"retrived":documents}
+
+def grade(state:RAGSubGraph):
+    
+    draft=state["draft"]
+    prompt=""
+    
