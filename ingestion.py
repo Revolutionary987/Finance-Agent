ec307cff2d909ebbf3697d667f5b17fdb3038f95
Revolutionary987 <busygodyo@gmail.com>
2026-05-30 08:36:56 +0530

    changed embedding model?

--- ingestion.py
+++ ingestion.py
@@ -130,7 +130,14 @@ class Ingestion:
         return langchain_documents
     
     def embedding(self,langchain_documents,persist_directory="ddb/chroma_db"):
-        embedding_model = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
+        model_name="BAAI/bge-m3"
+        model_kwargs={"device":"cpu"}
+        encode_kwargs={"normalize_embeddings":True}
+        embedding_model=HuggingFaceEmbeddings(
+            model_name=model_name,
+            model_kwargs=model_kwargs,
+            encode_kwargs=encode_kwargs
+        )
         vector_db=Chroma.from_documents(
             documents=langchain_documents,
             embedding=embedding_model,
